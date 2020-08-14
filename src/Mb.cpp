8ed33421d45c47629c93c9db0b66f84ad89623d4
ben@kristall-klang.de <ben@kristall-klang.de>
2020-08-14 22:11:25 +0200

    Mb - added import/export

--- src/Mb.cpp
+++ src/Mb.cpp
@@ -2,9 +2,9 @@
 #include "Mb.hpp"
 #include "Mb_v1.hpp"
 #include "Mb_v06.hpp"
+#include <osdialog.h>
 
 namespace Mb {
-
 
 BrowserOverlay::BrowserOverlay() {
 	v1::modelBoxZoom = pluginSettings.mbV1zoom;
@@ -138,9 +138,116 @@ struct MbWidget : ModuleWidget {
 			}
 		};
 
+		struct ExportItem : MenuItem {
+			MbWidget* mw;
+			void onAction(const event::Action& e) override {
+				mw->exportSettingsDialog();
+			}
+		};
+
+		struct ImportItem : MenuItem {
+			MbWidget* mw;
+			void onAction(const event::Action& e) override {
+				mw->importSettingsDialog();
+			}
+		};
+
 		menu->addChild(new MenuSeparator());
 		menu->addChild(construct<ModeV06Item>(&MenuItem::text, "v0.6", &ModeV06Item::module, module));
 		menu->addChild(construct<ModeV1Item>(&MenuItem::text, "v1 mod", &ModeV1Item::module, module));
+		menu->addChild(new MenuSeparator());
+		menu->addChild(construct<ExportItem>(&MenuItem::text, "Export favorites/hidden", &ExportItem::mw, this));
+		menu->addChild(construct<ImportItem>(&MenuItem::text, "Import favorites/hidden", &ImportItem::mw, this));
+	}
+
+	void exportSettings(std::string filename) {
+		INFO("Saving settings %s", filename.c_str());
+
+		json_t* rootJ = moduleBrowserToJson();
+
+		DEFER({
+			json_decref(rootJ);
+		});
+
+		FILE* file = fopen(filename.c_str(), "w");
+		if (!file) {
+			std::string message = string::f("Could not write to patch file %s", filename.c_str());
+			osdialog_message(OSDIALOG_WARNING, OSDIALOG_OK, message.c_str());
+		}
+		DEFER({
+			fclose(file);
+		});
+
+		json_dumpf(rootJ, file, JSON_INDENT(2) | JSON_REAL_PRECISION(9));
+	}
+
+	void exportSettingsDialog() {
+		osdialog_filters* filters = osdialog_filters_parse(":json");
+		DEFER({
+			osdialog_filters_free(filters);
+		});
+
+		char* path = osdialog_file(OSDIALOG_SAVE, "", "stoermelder-mb.json", filters);
+		if (!path) {
+			// No path selected
+			return;
+		}
+		DEFER({
+			free(path);
+		});
+
+		std::string pathStr = path;
+		std::string extension = string::filenameExtension(string::filename(pathStr));
+		if (extension.empty()) {
+			pathStr += ".json";
+		}
+
+		exportSettings(pathStr);
+	}
+
+	void importSettings(std::string filename) {
+		INFO("Loading settings %s", filename.c_str());
+
+		FILE* file = fopen(filename.c_str(), "r");
+		if (!file) {
+			std::string message = string::f("Could not load file %s", filename.c_str());
+			osdialog_message(OSDIALOG_WARNING, OSDIALOG_OK, message.c_str());
+			return;
+		}
+		DEFER({
+			fclose(file);
+		});
+
+		json_error_t error;
+		json_t* rootJ = json_loadf(file, 0, &error);
+		if (!rootJ) {
+			std::string message = string::f("File is not a valid file. JSON parsing error at %s %d:%d %s", error.source, error.line, error.column, error.text);
+			osdialog_message(OSDIALOG_WARNING, OSDIALOG_OK, message.c_str());
+			return;
+		}
+		DEFER({
+			json_decref(rootJ);
+		});
+
+		moduleBrowserFromJson(rootJ);
+	}
+
+	void importSettingsDialog() {
+		osdialog_filters* filters = osdialog_filters_parse(":json");
+		DEFER({
+			osdialog_filters_free(filters);
+		});
+
+		char* path = osdialog_file(OSDIALOG_OPEN, "", NULL, filters);
+		if (!path) {
+			// No path selected
+			return;
+		}
+		DEFER({
+			free(path);
+		});
+
+		importSettings(path);
 	}
 };
 
