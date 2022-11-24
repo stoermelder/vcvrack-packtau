f8d30e0e86b9d81b36c86314156fb882c1b465f2
stoermelder <ben@kristall-klang.de>
2022-11-24 19:14:16 +0100

    Azure pipeline

--- scripts/release-notes.sh
+++ scripts/release-notes.sh
@@ -6,9 +6,14 @@ cat <<- EOH
 This build is automatically generated every time I push my changes. As such it is the latest version of the code and may be unstable, unusable, unsuitable for human consumption, and so on.
 
 These assets were built against
-https://vcvrack.com/downloads/Rack-SDK-2.0.6-win.zip
-https://vcvrack.com/downloads/Rack-SDK-2.0.6-mac.zip
-https://vcvrack.com/downloads/Rack-SDK-2.0.6-lin.zip
+https://vcvrack.com/downloads/Rack-SDK-2.1.0-win.zip
+https://vcvrack.com/downloads/Rack-SDK-2.1.0-mac.zip
+https://vcvrack.com/downloads/Rack-SDK-2.1.0-lin.zip
+
+Used build environments:
+macos-11
+windows-2019
+ubuntu-20.04
 
 The build date and most recent commits are:
 EOH
